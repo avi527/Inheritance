288be0f2322a257ca0ad7de317e40be467b78c27
avi527 <54246200+avi527@users.noreply.github.com>
2019-08-23 00:34:59 +0530

    Add files via upload

---
+++ inherit_parent_class.py
@@ -0,0 +1,24 @@
+class Phone:
+    def __init__(self,brand,model_name,price):
+        self.brand=brand
+        self.model_name=model_name
+        self.price=price
+
+    def full_name(self):
+        return f"{self.brand} {self.model_name}"
+
+    def make_a_call(self,number):
+        return f"calling {number}...."
+
+class Smartphone(Phone):
+    def __init__(self,brand,model_name,price,ram,internal_memory,rear_camera):
+        super().__init__(brand,model_name,price)
+        self.ram=ram
+        self.internal_memory=internal_memory
+        self.rear_camera=rear_camera
+
+phone=Phone('nokia','1100',1000)
+smartphone=Smartphone('oneplus','5',3000,'64 gb','64gb','20mb')
+print(phone.full_name())
+print(smartphone.full_name())
+print(smartphone.rear_camera)
